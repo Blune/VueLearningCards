5e2090bfdccadfec1586526e932998ea373d011d
Blune <blune@web.de>
2021-10-12 18:47:50 +0200

    added json content loading

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,42 +1,51 @@
 <script>
+import cardData from "../cards.json";
+
 export default {
   data(){
     return {
       flipped: false,
-      front: 'Who wrote this?',
-      back: 'me',
+      index: 0,
+      cards: cardData.cards
     }
   },
    methods: {
-    handleClick: function(){
+    flipCard: function(){
     this.flipped = !this.flipped;
+    },
+    next: function(){
+      this.index = Math.min(this.index+1, this.cards.length -1);
+    },
+    previous: function(){
+      this.index = Math.max(this.index-1, 0);
     }
-  }  
+  },
+  computed: {
+    front(){
+      return this.cards[this.index].front;
+    },
+    back(){
+      return this.cards[this.index].back;
+    }
+  }
 }
 </script>
 
 <template>
-  <div class="center" >
+  <div class="card" @click="flipCard">
     <h2 v-if="!flipped" v-text="front"></h2>
-    <h2 v-else v-text="this.back"></h2>
-    <button @click="handleClick">Flip</button>
-
+    <h2 v-else v-text="back"></h2>
+  </div>
+  <div>
+    <button @click="previous">prev</button>
+    <button @click="next">next</button>
   </div>
 </template>
 
 <style scoped>
-.center {
-  margin: auto;
-  width: 500px;
-  height: 300px;
-  padding: 10px;
-}
 
-button {
-  position: relative;
-  bottom:   -100px;
-}
-div {
+
+.card {
   background-color: bisque;
   width: 500px;
   height: 300px;
@@ -44,5 +53,7 @@ div {
   display: inline-block;
   text-align: center;
   vertical-align: middle;
+  margin: auto;
+  padding: 10px;
 }
 </style>
